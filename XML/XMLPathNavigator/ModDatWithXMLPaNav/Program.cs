7a703b5d0beea6d808fe217ec991cd5fc19ee760
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-07-14 21:16:38 -0700

    Modifying an XML
    Sample code to modify an xml.

---
+++ XML/XMLPathNavigator/ModDatWithXMLPaNav/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+namespace ModDatWithXMLPaNav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load("MySmlFile.xml");
+            XPathNavigator navigator = document.CreateNavigator();
+
+            XmlNamespaceManager manager = new XmlNamespaceManager(navigator.NameTable);
+            manager.AddNamespace("bk", "http://www.contoso.com/books");
+
+            foreach (XPathNavigator nav in navigator.Select("//bk:price", manager))
+            {
+                if (nav.Value == "11.99")
+                {
+                    nav.SetValue("12.99");
+                }
+            }
+
+            Console.WriteLine(navigator.OuterXml);
+        }
+    }
+}
